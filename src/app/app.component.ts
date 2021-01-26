5b55f5c8869c24bcfdc4716774826004b9b6f5f2
Oranit Dar <oranit.dar@gmail.com>
2021-01-26 20:07:57 +0200

    Changed title again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title:string = 'gocode-shop/gccode';
+  title:string = 'gocode-shop or gccode';
   onToggle() {
     this.title = this.title ? '' : 'gocode-shop';
   }
