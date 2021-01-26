814b2f06a76e9d520296236c24edb4414267d083
Oranit Dar <oranit.dar@gmail.com>
2021-01-26 18:33:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title:string = 'gocode-shop';
+  onToggle() {
+    this.title = this.title ? '' : 'gocode-shop';
+  }
+  changeTitle(e: any) {
+    this.title = (<HTMLInputElement>e.target).value;
+  }
+}
